401b278252d9944269effa442b6df98e9ebb6db9
Beatriz Carvalho <biacarvalho1998@outlook.com>
2022-09-17 13:22:31 -0300

    Primeiro Commit

---
+++ desafiotecnicoqred/src/main/java/com/desafiotecnicoqred/api/DesafiotecnicoqredApplication.java
@@ -0,0 +1,13 @@
+package com.desafiotecnicoqred.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiotecnicoqredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiotecnicoqredApplication.class, args);
+	}
+
+}
---
+++ desafiotecnicoqred/src/test/java/com/desafiotecnicoqred/api/DesafiotecnicoqredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafiotecnicoqred.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiotecnicoqredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
